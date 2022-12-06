098811a02f55bd92a7935e530ebfcf50a422c8bc
Farkhod.Khalilov <farkhod.khalilov.94@mail.ru>
2022-12-06 14:48:29 +0500

    first commit

---
+++ src/main/java/com/example/appcommunicationcompany/AppCommunicationCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.appcommunicationcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCommunicationCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCommunicationCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appcommunicationcompany/AppCommunicationCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appcommunicationcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCommunicationCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
